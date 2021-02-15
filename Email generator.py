d81fcd47d5500470d4870a15d2683a683ca4372b
BellVirtual <79067920+BellVirtual@users.noreply.github.com>
2021-02-14 22:17:22 -0600

    Initial Comment

---
+++ Email generator.py
@@ -0,0 +1,9 @@
+import random
+
+na = ["johnny", "samantha", "jerome", "Bernard", "Jeramia", "aples", "pens", "Pencils"]
+
+na1 = random.choice(na)
+
+nu = random.randint(1, 100)
+
+print(str(na1) + str(nu) + "@gmail.com")
